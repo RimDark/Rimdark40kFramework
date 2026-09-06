d9d1725482ffc8f3837b8be67f6140c7a7479cb2
Phonicmas <jkskovlund@gmail.com>
2026-09-06 21:42:31 +0200

    require rank to use comp

---
+++ 1.6/Source/Core40k/Comps/CompAbilityEffect_RequireRank.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace Core40k;
+
+public class CompAbilityEffect_RequireRank : CompAbilityEffect
+{
+    private new CompProperties_AbilityRequireRank Props => (CompProperties_AbilityRequireRank)props;
+
+    private string RequirementLabel
+    {
+        get
+        {
+            var parts = Props.requiredRanksAll.Select(rank => rank.label).ToList();
+            if (!Props.requiredRanksOneAmong.NullOrEmpty())
+            {
+                parts.Add(Props.requiredRanksOneAmong.Select(rank => rank.label).ToCommaListOr());
+            }
+            return parts.ToCommaList();
+        }
+    }
+
+    /// <summary>
+    /// True when the pawn currently holds every rank in requiredRanksAll and at least one rank in requiredRanksOneAmong.
+    /// A pawn without a CompRankInfo only passes if no ranks are required.
+    /// </summary>
+    public static bool MeetsRequirement(Pawn pawn, CompProperties_AbilityRequireRank props)
+    {
+        var needsAll = !props.requiredRanksAll.NullOrEmpty();
+        var needsOneAmong = !props.requiredRanksOneAmong.NullOrEmpty();
+        if (!needsAll && !needsOneAmong)
+        {
+            return true;
+        }
+
+        var rankComp = pawn?.GetComp<CompRankInfo>();
+        if (rankComp == null)
+        {
+            return false;
+        }
+
+        if (needsAll && props.requiredRanksAll.Any(rank => !rankComp.HasRank(rank)))
+        {
+            return false;
+        }
+
+        return !needsOneAmong || props.requiredRanksOneAmong.Any(rankComp.HasRank);
+    }
+
+    public override bool GizmoDisabled(out string reason)
+    {
+        if (MeetsRequirement(parent.pawn, Props))
+        {
+            reason = null;
+            return false;
+        }
+
+        reason = "BEWH.Framework.Comp.CasterMissingRequiredRank".Translate(RequirementLabel);
+        return true;
+    }
+
+    public override bool Valid(LocalTargetInfo target, bool throwMessages = false)
+    {
+        if (MeetsRequirement(parent.pawn, Props))
+        {
+            return base.Valid(target, throwMessages);
+        }
+
+        if (throwMessages)
+        {
+            Messages.Message("BEWH.Framework.Comp.CasterMissingRequiredRank".Translate(RequirementLabel), parent.pawn, MessageTypeDefOf.RejectInput, false);
+        }
+        return false;
+    }
+}
---
+++ 1.6/Source/Core40k/Comps/CompProperties_AbilityRequireRank.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+
+namespace Core40k;
+
+public class CompProperties_AbilityRequireRank : CompProperties_AbilityEffect
+{
+    public List<RankDef> requiredRanksOneAmong = [];
+    public List<RankDef> requiredRanksAll = [];
+
+    public CompProperties_AbilityRequireRank()
+    {
+        compClass = typeof(CompAbilityEffect_RequireRank);
+    }
+}
